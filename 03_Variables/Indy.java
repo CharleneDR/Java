9a2a96f16afeb477b73f686a88b377261a3b0c31
CharleneDR <cdarugna@sqli.com>
2023-03-22 17:48:19 +0100

    Quête 3

---
+++ 03_Variables/Indy.java
@@ -0,0 +1,13 @@
+public class Indy {
+    public static void main(String[] args) {
+        String movieName = "Indiana Jones and the last Crusade";
+        boolean movieSeen = false;
+        int yearReleased = 1989;
+        float notation = 8.2f;
+
+        System.out.println(movieName);
+        System.out.println(movieSeen);
+        System.out.println(yearReleased);
+        System.out.println(notation);
+    }
+}
